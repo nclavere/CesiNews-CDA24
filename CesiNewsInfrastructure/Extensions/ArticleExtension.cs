7f9ce77f808daa2c6f54bf1b54b7e19287899813
Nadine CLAVERE <nadine.clavere@equadex.net>
2025-03-17 16:33:35 +0100

    add article extension

---
+++ CesiNewsInfrastructure/Extensions/ArticleExtension.cs
@@ -0,0 +1,24 @@
+﻿using CesiNewsInfrastructure.Dto;
+using CesiNewsModel.Entities;
+using System.Runtime.CompilerServices;
+
+namespace CesiNewsInfrastructure.Extensions;
+public static class ArticleExtension
+{
+    public static ArticleDto ToDto(this Article article)
+    {
+        return new ArticleDto
+        {
+            Id = article.Id,
+            Titre = article.Titre,
+            Resume = article.Resume,
+            Contenu = article.Contenu,
+            Auteur = article.Auteur,
+            DatePubli = article.DatePubli,
+            Support = article.Support?.Libelle,
+            Categories = article.Categories?.Select(c => c.Libelle).ToList()
+        };
+    }
+
+
+}
