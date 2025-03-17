7c4c60db3d21160043c635d7595d750d846253ad
Nadine CLAVERE <nadine.clavere@equadex.net>
2025-03-17 15:58:33 +0100

    ajout initial

---
+++ CesiNewsApi/CesiNewsModel/Context/NewsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CesiNewsModel.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CesiNewsModel.Context;
+public class NewsDbContext : DbContext
+{
+    public virtual DbSet<Categorie> Categories { get; set; } = null!;
+    public virtual DbSet<Article> Articles { get; set; } = null!;
+    public virtual DbSet<Support> Supports { get; set; } = null!;
+    public virtual DbSet<Texte> Textes { get; set; } = null!;
+    public virtual DbSet<Video> Videos { get; set; } = null!;
+
+    public NewsDbContext(DbContextOptions<NewsDbContext> options)
+        : base(options)
+    {
+    }
+
+
+}
---
+++ CesiNewsApi/CesiNewsModel/Entities/Support.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CesiNewsModel.Entities;
+public abstract class Support
+{
+    public int Id { get; set; }
+
+    [StringLength(40)]
+    public string Libelle { get; set; } = null!;
+}
